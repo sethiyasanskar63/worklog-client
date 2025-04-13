a484c85fa01e388b7fbe70893b7b58b84862b732
Prakhar Awasthi <awasthip65@gmail.com>
2025-04-13 09:26:13 +0530

    Release-1.0.0 : Styling and Validation
    Release-1.0.0 : Styling the elements of the UI and adding validation for ticket and form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,14 @@
 import { Component, Output, EventEmitter, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormControl, FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormsModule, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { TimeCard } from '../../domain/time-card.model';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { Ticket } from '../../domain/ticket.model';
 
 @Component({
@@ -17,7 +19,9 @@ import { Ticket } from '../../domain/ticket.model';
     FormsModule,
     MatAutocompleteModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
@@ -26,14 +30,26 @@ import { Ticket } from '../../domain/ticket.model';
 export class FormComponent implements OnInit, OnChanges {
   @Output() formDataSubmitted = new EventEmitter<TimeCard>();
   @Input() tickets: Ticket[] | { content: Ticket[] } = [];
-  
+
   ticketIdControl = new FormControl('');
   dateControl = new FormControl('');
   timeSpentControl = new FormControl('');
-  
+  timeForm: FormGroup;
+
   // Remove hardcoded values
   ticketOptions: string[] = [];
   filteredTicketOptions?: Observable<string[]>;
+  today: string;
+
+  constructor(private fb: FormBuilder) {
+    // Initialize ticketForm in the constructor
+    this.timeForm = this.fb.group({
+      ticketId: new FormControl('', [Validators.required]),
+      date: new FormControl('', [Validators.required]),
+      timeSpent: new FormControl('', [Validators.required]),
+    });
+    this.today = new Date().toISOString();
+  }
 
   ngOnInit() {
     this.setupFilteredOptions();
@@ -73,14 +89,14 @@ export class FormComponent implements OnInit, OnChanges {
 
   private _filterTickets(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return this.ticketOptions.filter(option => 
+    return this.ticketOptions.filter(option =>
       option.toLowerCase().includes(filterValue)
     );
   }
 
   onSubmit(event: Event) {
     event.preventDefault();
-    
+
     const selectedTicket = this.tickets instanceof Array
       ? this.tickets.find(ticket => `${ticket.ticketId} - ${ticket.name || 'Unnamed'}` === this.ticketIdControl.value)
       : this.tickets?.content?.find(ticket => `${ticket.ticketId} - ${ticket.name || 'Unnamed'}` === this.ticketIdControl.value);
